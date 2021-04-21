fd63472ff09cf8c9d825c5d8c0d4e87b7b4825e4
Fdrv <oldgot@gmail.com>
2021-04-21 21:00:11 +0300

    Add inline field edit and filtering

--- property/admin.py
+++ property/admin.py
@@ -7,6 +7,9 @@ from .models import Flat
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ['town', 'address', 'owner']
     readonly_fields = ['created_at']
+    list_display = ['address', 'price', 'new_building', 'created_at', 'town']
+    list_editable = ['new_building']
+    list_filter = ['new_building']
     pass
 
 # admin.site.register(Flat, FlatAdmin)
