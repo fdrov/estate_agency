e402ba6bb1d96e9d3c0dda8a7c7403d7aa13e606
Fdrv <oldgot@gmail.com>
2021-04-23 20:55:00 +0300

    Add admin filter

--- property/admin.py
+++ property/admin.py
@@ -10,7 +10,7 @@ class FlatAdmin(admin.ModelAdmin):
     list_display = ['address', 'price', 'new_building', 'construction_year',
                     'created_at', 'town']
     list_editable = ['new_building']
-    list_filter = ['new_building']
+    list_filter = ['new_building', 'rooms_number', 'has_balcony']
     pass
 
 # admin.site.register(Flat, FlatAdmin)
