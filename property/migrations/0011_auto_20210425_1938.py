75aade832597a758f7bb60495812b3bb23a258a4
Fdrv <oldgot@gmail.com>
2021-04-26 22:37:19 +0300

    Normalize Flat.phonenumbers into Flat.owner_pure_phone

--- property/migrations/0011_auto_20210425_1938.py
+++ property/migrations/0011_auto_20210425_1938.py
@@ -7,13 +7,14 @@ import phonenumbers
 def pure_phone_numbers(apps, schema_editor):
     Flat = apps.get_model('property', 'Flat')
     for flat in Flat.objects.all():
-        if not flat.owner_pure_phone:
-            pure_number = phonenumbers.parse(flat.owners_phonenumber, 'RU')
-            flat.owner_pure_phone = phonenumbers.format_number(
-                pure_number,
-                phonenumbers.PhoneNumberFormat.E164
-            )
-            flat.save()
+        pure_number = phonenumbers.parse(flat.owners_phonenumber, 'RU')
+        if not phonenumbers.is_valid_number(pure_number):
+            continue
+        flat.owner_pure_phone = phonenumbers.format_number(
+            pure_number,
+            phonenumbers.PhoneNumberFormat.E164
+        )
+        flat.save()
 
 
 class Migration(migrations.Migration):
