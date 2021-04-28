49dcd3530abaf77bc393803c2a4144fe03a44575
Fdrv <oldgot@gmail.com>
2021-04-29 00:16:32 +0300

    Add inline widget to flat detail view

--- property/admin.py
+++ property/admin.py
@@ -3,6 +3,11 @@ from django.contrib import admin
 from .models import Flat, Complaint, Owner
 
 
+class FlatInline(admin.TabularInline):
+    model = Flat.estate_owners.through
+    raw_id_fields = ['owner']
+
+
 @admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ['town', 'address']
@@ -10,19 +15,23 @@ class FlatAdmin(admin.ModelAdmin):
     list_display = ['address', 'new_building']
     fieldsets = (
         (None, {
-            'fields': ('address', 'description', 'price', 'town', 'town_district')
+            'fields': ('address', 'description', 'price',
+                       'town', 'town_district')
         }),
         ('Про здание', {
             'fields': ('floor', 'rooms_number', 'living_area', 'has_balcony')
         }),
         ('Мета-инфа', {
-            'fields': ('construction_year', 'new_building','active', 'likes', 'created_at')
+            'fields': ('construction_year', 'new_building',
+                       'active', 'likes', 'created_at')
         }),
     )
     list_filter = ['new_building', 'rooms_number', 'has_balcony']
     raw_id_fields = ['likes']
+    inlines = [FlatInline]
 
-@ admin.register(Complaint)
+
+@admin.register(Complaint)
 class ComplaintAdmin(admin.ModelAdmin):
     raw_id_fields = ['user', 'flat']
     pass
