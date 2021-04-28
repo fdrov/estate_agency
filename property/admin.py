6053cbb1ba4f2d5af3931015e9e2d11d71a53bb4
Fdrv <oldgot@gmail.com>
2021-04-28 23:40:03 +0300

    Some admin improvements

--- property/admin.py
+++ property/admin.py
@@ -7,13 +7,22 @@ from .models import Flat, Complaint, Owner
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ['town', 'address']
     readonly_fields = ['created_at']
-    list_display = ['address']
-    # list_editable = ['new_building']
-    # list_filter = ['new_building', 'rooms_number', 'has_balcony']
+    list_display = ['address', 'new_building']
+    fieldsets = (
+        (None, {
+            'fields': ('address', 'description', 'price', 'town', 'town_district')
+        }),
+        ('Про здание', {
+            'fields': ('floor', 'rooms_number', 'living_area', 'has_balcony')
+        }),
+        ('Мета-инфа', {
+            'fields': ('construction_year', 'new_building','active', 'likes', 'created_at')
+        }),
+    )
+    list_filter = ['new_building', 'rooms_number', 'has_balcony']
     raw_id_fields = ['likes']
 
-
-@admin.register(Complaint)
+@ admin.register(Complaint)
 class ComplaintAdmin(admin.ModelAdmin):
     raw_id_fields = ['user', 'flat']
     pass
@@ -22,5 +31,5 @@ class ComplaintAdmin(admin.ModelAdmin):
 @admin.register(Owner)
 class OwnerAdmin(admin.ModelAdmin):
     raw_id_fields = ['estates']
-    list_display = ['full_name', 'display_estates' ]
+    list_display = ['full_name', 'display_estates']
     search_fields = ['full_name']
